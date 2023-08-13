b5d8a7ab894fb53c8cc2acb2ceead13562f644c9
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-08-13 22:22:40 +0300

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    NotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
