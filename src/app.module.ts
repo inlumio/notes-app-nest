33c139b1fc500c1328ca1acb7973a3a1f7c3c978
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-08-13 22:48:54 +0300

    install sequalize and base settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        port: configService.get('database.port'),
+        host: configService.get('database.host'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        synchronize: true,
+        autoLoadModels: true,
+        models: [],
+      }),
+    }),
     NotesModule,
   ],
   controllers: [AppController],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,10 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    name: process.env.DB_NAME || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASSWORD || 'postgres',
+  },
 });
